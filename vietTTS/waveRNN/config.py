01a5388ddac0cbc2377d3bae8ca4b25221990b4b
ntt123 <ntt123@earth>
2021-06-03 11:46:03 +0800

    small batch size causes nan

--- vietTTS/waveRNN/config.py
+++ vietTTS/waveRNN/config.py
@@ -13,7 +13,7 @@ class FLAGS:
   gru_dim = 896
   embed_dim = 896
   _training_schedule = [
-      TrainingConfig(1_000,     1e-6,   1),
+      TrainingConfig(1_000,     1e-6,   8),
       TrainingConfig(2_000,     1e-5,  16),
       TrainingConfig(3_000,     1e-4,  32),
       TrainingConfig(5_000,     3e-4,  64),
