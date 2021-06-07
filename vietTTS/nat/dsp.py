fix upsample, fix mel, data aug w/ random scaling, true llh

vietTTS/nat/dsp.py
@@ -111,7 +111,7 @@ class MelFilter:
     p = (self.n_fft - hop_length) // 2
     y = jnp.pad(y, ((p, p), (0, 0)), mode='reflect')
     spec = batched_stft(y, self.n_fft, hop_length, window_length, 'hann', False, 'reflect')
-    mag = jnp.sqrt(jnp.square(spec.real) + jnp.square(spec.imag) + 1e-9)
+    mag = jnp.sqrt(jnp.square(spec.real) + jnp.square(spec.imag))
     mel = jnp.einsum('ms,sfn->nfm', self.melfb, mag)
-    cond = jnp.log(jnp.clip(mel, a_min=1e-5, a_max=None))
+    cond = jnp.log(mel + 1e-5)
     return cond

vietTTS/waveRNN/model.py
@@ -15,9 +15,9 @@ class UpsampleNetwork(hk.Module):
     self.dilated_conv_2 = hk.Conv1D(512, 2, 1, rate=4, padding='VALID')
     self.dilated_bn_2 = hk.BatchNorm(True, True, 0.99)
 
-    self.upsample_conv_1 = hk.Conv1DTranspose(512, kernel_shape=1, stride=2, padding='SAME')
+    self.upsample_conv_1 = hk.Conv1DTranspose(512, kernel_shape=1, stride=4, padding='SAME')
     self.upsample_bn_1 = hk.BatchNorm(True, True, 0.99)
-    self.upsample_conv_2 = hk.Conv1DTranspose(512, kernel_shape=1, stride=2, padding='SAME')
+    self.upsample_conv_2 = hk.Conv1DTranspose(512, kernel_shape=1, stride=4, padding='SAME')
     self.upsample_bn_2 = hk.BatchNorm(True, True, 0.99)
     self.upsample_conv_3 = hk.Conv1DTranspose(num_output_channels, kernel_shape=1, stride=4, padding='SAME')
     self.upsample_bn_3 = hk.BatchNorm(True, True, 0.99)
@@ -36,7 +36,7 @@ class UpsampleNetwork(hk.Module):
 
     # tile x16
     N, L, D = x.shape
-    x = jnp.tile(x[:, :, None, :], (1, 1, 16, 1))
+    x = jnp.tile(x[:, :, None, :], (1, 1, 4, 1))
     x = jnp.reshape(x, (N, -1, D))
 
     return x

vietTTS/waveRNN/trainer.py
@@ -33,7 +33,7 @@ def loss_fn(params, aux, batch, sr=16000):
   variance = jnp.sum(jnp.square(v - mean) * pr, axis=-1, keepdims=True)
   reg = jnp.log(1 + jnp.sqrt(variance))
   targets = jax.nn.one_hot(mutargets, num_classes=256, axis=-2)
-  llh = jnp.sum(targets * logpr, axis=-2)
+  llh = jnp.sum(targets * logpr, axis=[-2, -1])
   l1 = -jnp.mean(llh)
   l2 = FLAGS.variance_loss_scale * jnp.mean(reg)
   return l1 + l2, (l1, l2, aux)