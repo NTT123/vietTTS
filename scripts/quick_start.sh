601f3f4650cb72ac1eedc101396795b58ea4d273
ntt123 <ntt123@earth>
2021-07-09 23:04:14 +0800

    update latest models

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,10 +1,10 @@
-if [ ! -f assets/infore/hifigan/g_00710000 ]; then
+if [ ! -f assets/infore/hifigan/g_01135000 ]; then
   pip3 install gdown
   echo "Downloading models..."
   mkdir -p assets/infore/{nat,hifigan}
   gdown --id 1zh6axidF1H9TAz9t45IrSe_mQ8KZMgNr -O assets/infore/nat/nat_ckpt_latest.pickle
-  gdown --id 10dsJ7zO7qdUHGzAzWRa9fepHgaect4ey -O assets/infore/hifigan/g_00710000
-  python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_00710000
+  gdown --id 1-NTQc4RDK-DgBIQVk-t4dgtYI1MUbBIk -O assets/infore/hifigan/g_01135000
+  python3 -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_01135000
 fi
 
 echo "Generate audio clip"
