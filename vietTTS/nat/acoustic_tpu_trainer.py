a6c98ffda9d2375e5a90ee58ee456ce92c0edefa
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-12-21 11:07:33 +0000

    v0.3.3: train acoustic model on TPU.

--- vietTTS/nat/acoustic_tpu_trainer.py
+++ vietTTS/nat/acoustic_tpu_trainer.py
@@ -1,101 +1,82 @@
+import os
 import pickle
 from functools import partial
 from typing import Deque
 
-import haiku as hk
+import fire
 import jax
 import jax.numpy as jnp
+import jax.tools.colab_tpu
 import matplotlib.pyplot as plt
 import optax
 from tqdm.auto import tqdm
-from vietTTS.nat.config import AcousticInput
 
-from .config import FLAGS, AcousticInput
+from .acoustic_trainer import initial_state, loss_vag, val_loss_fn
+from .config import FLAGS
 from .data_loader import load_textgrid_wav
 from .dsp import MelFilter
-from .model import AcousticModel
 from .utils import print_flags
 
 
-@hk.transform_with_state
-def net(x):
-    return AcousticModel(is_training=True)(x)
+def setup_colab_tpu():
+    jax.tools.colab_tpu.setup_tpu()
 
 
-@hk.transform_with_state
-def val_net(x):
-    return AcousticModel(is_training=False)(x)
-
-
-def loss_fn(params, aux, rng, inputs: AcousticInput, is_training=True):
-    melfilter = MelFilter(
-        FLAGS.sample_rate, FLAGS.n_fft, FLAGS.mel_dim, FLAGS.fmin, FLAGS.fmax
-    )
-    mels = melfilter(inputs.wavs.astype(jnp.float32) / (2 ** 15))
-    B, L, D = mels.shape
-    go_frame = jnp.zeros((B, 1, D), dtype=jnp.float32)
-    inp_mels = jnp.concatenate((go_frame, mels[:, :-1, :]), axis=1)
-    n_frames = inputs.durations * FLAGS.sample_rate / (FLAGS.n_fft // 4)
-    inputs = inputs._replace(mels=inp_mels, durations=n_frames)
-    model = net if is_training else val_net
-    (mel1_hat, mel2_hat), new_aux = model.apply(params, aux, rng, inputs)
-    loss1 = (jnp.square(mel1_hat - mels) + jnp.square(mel2_hat - mels)) / 2
-    loss2 = (jnp.abs(mel1_hat - mels) + jnp.abs(mel2_hat - mels)) / 2
-    loss = jnp.mean((loss1 + loss2) / 2, axis=-1)
-    num_frames = (inputs.wav_lengths // (FLAGS.n_fft // 4))[:, None]
-    mask = jnp.arange(0, L)[None, :] < num_frames
-    loss = jnp.sum(loss * mask) / jnp.sum(mask)
-    return (loss, new_aux) if is_training else (loss, new_aux, mel2_hat, mels)
-
-
-train_loss_fn = partial(loss_fn, is_training=True)
-val_loss_fn = jax.jit(partial(loss_fn, is_training=False))
-
-loss_vag = jax.value_and_grad(train_loss_fn, has_aux=True)
-
-
-def initial_state(optimizer, batch):
-    rng = jax.random.PRNGKey(42)
-    params, aux = hk.transform_with_state(lambda x: AcousticModel(True)(x)).init(
-        rng, batch
-    )
-    optim_state = optimizer.init(params)
-    return params, aux, rng, optim_state
-
-
-def train():
+def train(
+    batch_size: int = 32,
+    steps_per_update: int = 10,
+    learning_rate: float = 1024e-6,
+):
+    """Train acoustic model on multiple cores (TPU)."""
+    lr_schedule = optax.exponential_decay(learning_rate, 50_000, 0.5, staircase=True)
 
     optimizer = optax.chain(
         optax.clip_by_global_norm(1.0),
-        optax.adamw(FLAGS.learning_rate, weight_decay=FLAGS.weight_decay),
+        optax.adamw(lr_schedule, weight_decay=FLAGS.weight_decay),
     )
 
-    @jax.jit
-    def update(params, aux, rng, optim_state, inputs):
+    def update_step(prev_state, inputs):
+        params, aux, rng, optim_state = prev_state
         rng, new_rng = jax.random.split(rng)
         (loss, new_aux), grads = loss_vag(params, aux, rng, inputs)
+        grads = jax.lax.pmean(grads, axis_name="i")
         updates, new_optim_state = optimizer.update(grads, optim_state, params)
-        new_params = optax.apply_updates(updates, params)
-        return loss, (new_params, new_aux, new_rng, new_optim_state)
+        new_params = optax.apply_updates(params, updates)
+        next_state = (new_params, new_aux, new_rng, new_optim_state)
+        return next_state, loss
 
+    @partial(jax.pmap, axis_name="i")
+    def update(params, aux, rng, optim_state, inputs):
+        states, losses = jax.lax.scan(
+            update_step, (params, aux, rng, optim_state), inputs
+        )
+        return states, jnp.mean(losses)
+
+    print(jax.devices())
+    num_devices = jax.device_count()
     train_data_iter = load_textgrid_wav(
         FLAGS.data_dir,
         FLAGS.max_phoneme_seq_len,
-        FLAGS.batch_size,
+        batch_size * num_devices * steps_per_update,
         FLAGS.max_wave_len,
         "train",
     )
     val_data_iter = load_textgrid_wav(
         FLAGS.data_dir,
         FLAGS.max_phoneme_seq_len,
-        FLAGS.batch_size,
+        batch_size,
         FLAGS.max_wave_len,
         "val",
     )
     melfilter = MelFilter(
-        FLAGS.sample_rate, FLAGS.n_fft, FLAGS.mel_dim, FLAGS.fmin, FLAGS.fmax
+        FLAGS.sample_rate,
+        FLAGS.n_fft,
+        FLAGS.mel_dim,
+        FLAGS.fmin,
+        FLAGS.fmax,
     )
     batch = next(train_data_iter)
+    batch = jax.tree_map(lambda x: x[:1], batch)
     batch = batch._replace(mels=melfilter(batch.wavs.astype(jnp.float32) / (2 ** 15)))
     params, aux, rng, optim_state = initial_state(optimizer, batch)
     losses = Deque(maxlen=1000)
@@ -104,7 +85,7 @@ def train():
     last_step = -1
 
     # loading latest checkpoint
-    ckpt_fn = FLAGS.ckpt_dir / "acoustic_ckpt_latest.pickle"
+    ckpt_fn = FLAGS.ckpt_dir / "acoustic_latest_ckpt.pickle"
     if ckpt_fn.exists():
         print("Resuming from latest checkpoint at", ckpt_fn)
         with open(ckpt_fn, "rb") as f:
@@ -118,14 +99,26 @@ def train():
             )
 
     tr = tqdm(
-        range(last_step + 1, FLAGS.num_training_steps + 1),
+        range(last_step + 1, FLAGS.num_training_steps + 1, steps_per_update),
         desc="training",
-        total=FLAGS.num_training_steps + 1,
-        initial=last_step + 1,
+        total=FLAGS.num_training_steps // steps_per_update + 1,
+        initial=last_step // steps_per_update + 1,
     )
+
+    params, aux, rng, optim_state = jax.device_put_replicated(
+        (params, aux, rng, optim_state), jax.devices()
+    )
+
+    def batch_reshape(batch):
+        return jax.tree_map(
+            lambda x: jnp.reshape(x, (num_devices, steps_per_update, -1) + x.shape[1:]),
+            batch,
+        )
+
     for step in tr:
         batch = next(train_data_iter)
-        loss, (params, aux, rng, optim_state) = update(
+        batch = batch_reshape(batch)
+        (params, aux, rng, optim_state), loss = update(
             params, aux, rng, optim_state, batch
         )
         losses.append(loss)
@@ -133,7 +126,7 @@ def train():
         if step % 10 == 0:
             val_batch = next(val_data_iter)
             val_loss, val_aux, predicted_mel, gt_mel = val_loss_fn(
-                params, aux, rng, val_batch
+                *jax.tree_map(lambda x: x[0], (params, aux, rng)), val_batch
             )
             val_losses.append(val_loss)
             attn = jax.device_get(val_aux["acoustic_model"]["attn"])
@@ -141,7 +134,7 @@ def train():
             gt_mel = jax.device_get(gt_mel[0])
 
         if step % 1000 == 0:
-            loss = sum(losses).item() / len(losses)
+            loss = jnp.mean(sum(losses)).item() / len(losses)
             val_loss = sum(val_losses).item() / len(val_losses)
             tr.write(f"step {step}  train loss {loss:.3f}  val loss {val_loss:.3f}")
 
@@ -159,21 +152,36 @@ def train():
 
             # saving checkpoint
             with open(ckpt_fn, "wb") as f:
+                params_, aux_, rng_, optim_state_ = jax.tree_map(
+                    lambda x: x[0], (params, aux, rng, optim_state)
+                )
                 pickle.dump(
                     {
                         "step": step,
-                        "params": params,
-                        "aux": aux,
-                        "rng": rng,
-                        "optim_state": optim_state,
+                        "params": params_,
+                        "aux": aux_,
+                        "rng": rng_,
+                        "optim_state": optim_state_,
                     },
                     f,
                 )
 
 
 if __name__ == "__main__":
+    # we don't use these flags.
+    del FLAGS.batch_size
+    del FLAGS.learning_rate
+    del FLAGS.duration_learning_rate
+    del FLAGS.duration_lstm_dim
+    del FLAGS.duration_embed_dropout_rate
+
     print_flags(FLAGS.__dict__)
+
+    if "COLAB_TPU_ADDR" in os.environ:
+        setup_colab_tpu()
+
     if not FLAGS.ckpt_dir.exists():
         print("Create checkpoint dir at", FLAGS.ckpt_dir)
         FLAGS.ckpt_dir.mkdir(parents=True, exist_ok=True)
-    train()
+
+    fire.Fire(train)
